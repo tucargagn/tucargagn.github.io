20684db07b7873e24c6473c89465607dd9bea00f
Jose M Viloria <josevill.linuxusr@gmail.com>
2015-03-25 21:16:37 -0430

    Final

---
+++ contax/gracias.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<head>
+	<title>TucargaGN - Gracias por su registro</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+	<div id="page-wrap">
+		<h1>Hemos enviado su registro a nuestras direcciones, estaremos en contacto con usted en la mayor prontitud posible.</h1><br />
+	</div>
+</body>
+</html>
